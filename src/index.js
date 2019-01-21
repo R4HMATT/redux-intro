a1234e22ccc13a0439467158d2a8d9097cdeb76a
Rahmatullah Nikyar <rahmat.nikyar@mail.utoronto.ca>
2019-01-20 19:00:59 -0500

    redux intro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import Counter from './Counter';
+import {Provider} from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState = {
+    count: 0
+  };
+  
+  function reducer(state = initialState, action) {
+    console.log("the initial state default value being passed into\
+    reducer");
+    console.log(state);
+    console.log(action)
+    switch(action.type) {
+      case 'INCREMENT':
+        return {
+          count: state.count + 1
+        };
+    
+      case 'DECREMENT':
+        return {
+          count: state.count - 1
+        }
+
+      default:
+        return state;
+    }
+  }
+
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+ </Provider>
+);
+
+render(<App />, document.getElementById('root'));
